3150ea0c41c5835865b2446526f664f6a22fd9e4
root <root@kvm2700.bero-host.de>
2021-04-11 00:41:37 +0200

    register site

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,12 +1,10 @@
-@extends('layouts.app')
+@extends('layouts.main')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
@@ -18,9 +16,9 @@
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <br><span style="color:lightgreen"  class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong><br>
+                                    </span><br>
                                 @enderror
                             </div>
                         </div>
@@ -32,9 +30,9 @@
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <br><span style="color:lightgreen"  class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong><br>
+                                    </span><br>
                                 @enderror
                             </div>
                         </div>
@@ -46,9 +44,9 @@
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <br><span style="color:lightgreen"  class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong><br>
+                                    </span><br>
                                 @enderror
                             </div>
                         </div>
@@ -63,12 +61,16 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button class="button-Glass" type="submit" class="btn btn-primary">
                                     {{ __('Register') }}
                                 </button>
                             </div>
                         </div>
+                        <a href="{{ route('login') }}">
+                            <input type="button" value="Login" /><br>
+                        </a>
                     </form>
+                    
                 </div>
             </div>
         </div>
