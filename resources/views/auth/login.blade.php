56bb701db2b621bd4bb51868df8fed96589cd5dd
root <root@kvm2700.bero-host.de>
2021-04-11 00:30:59 +0200

    login site

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,20 +3,20 @@
 @section('content')
 <div class="container">
                 
-                    <form method="POST" action="{{ route('login') }}">
+                    <form class="form-login" method="POST" action="{{ route('login') }}">
                         @csrf
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
                            
 
                             <div class="col-md-6">
-                           
+                            @error('email')
+                                    <br><span style="color:lightgreen"  class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong><br>
+                                    </span><br>
+                                @enderror
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                               
                             </div>
 
                         <div class="form-group row">
@@ -36,30 +36,38 @@
                         <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    
 
-                                    <label class="form-check-label" for="remember">
+                                    <input style="transform: scale(2); width:auto " type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label style="margin-top:500px !important" class="form-check-label" for="remember">
+                                    
                                         {{ __('Remember Me') }}
                                     </label>
                                 </div>
                             </div>
                         </div>
 
+                   
+
+
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                
-                            
-                                <button class="button-Glass">
+                            <button class="button-Glass">
                                     {{ __('Login') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
+                                    <br><a  href="{{ route('password.request') }}">
+                                        <input style="margin-bottom:0px" type="button" value="Reset Password" /><br>
+                                    </a><br>
                                 @endif
+                                <a href="{{ route('register') }}">
+                                     <input style="margin-top:0px" type="button" value="Register" /><br>
+                                </a>
                             </div>
                         </div>
+                        <br>
                     </form>
                 <div class="drops">
                     <div class="drop drop-1"></div>
